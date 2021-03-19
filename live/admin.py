7f0265d8d64da49336a0cf65a5fa2f280e399bb2
Omar <omarnina@gmail.com>
2021-03-19 18:33:05 -0400

    only models

--- live/admin.py
+++ live/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Product, Location, Profile, Comment, Status, Log, Profile_Location, Location_User
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_name', 'item_code', 'item_cod_desc')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'detail', 'date', 'status')
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_id', 'status_id')
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'status')
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'level', 'item_code', 'item_cod_desc')
+
+class Prof_Loc_Admin(admin.ModelAdmin):
+    list_display = ('id', 'profile_id', 'location_id', 'step')
+
+class Location_User_Admin(admin.ModelAdmin):
+     list_display = ('id', 'location_id', 'user_id')
+
+
+##### Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Log, LogAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Profile_Location, Prof_Loc_Admin)
+admin.site.register(Location_User, Location_User_Admin)
--- live/models.py
+++ live/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    so_line = models.CharField(max_length=14)
+    customer_no = models.CharField(max_length=15)
+    sales_order_no = models.CharField(max_length=7)
+    customer_name = models.CharField(max_length=50)
+    linekey = models.CharField(max_length=6)
+    item_code = models.CharField(max_length=30)
+    item_cod_desc = models.CharField(max_length=64)
+    so_header = models.DateField()
+    prom_date = models.DateField()
+    clear_date = models.DateField()
+    exp_date_i = models.DateField()
+    exp_date_f = models.DateField()
+    qty = models.IntegerField()
+    qty_shipped = models.IntegerField()
+    unit_price = models.CharField(max_length=20)
+    production_notes = models.CharField(max_length=250)
+    qty_on_hand = models.IntegerField()
+    rma_number = models.IntegerField()
+    drop_ship = models.CharField(max_length=2)
+    comments = models.CharField(max_length=250)
+    def __str__(self):
+        return f"{self.so_line} {self.customer_name} {self.item_code} {self.item_cod_desc}"
+
+class Comment(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="product_comment")
+    detail = models.CharField(max_length=64)
+    date = models.DateField()
+    status = models.BooleanField(default=1)
+    def __str__(self):
+        return f"{self.id} {self.profile_id} {self.detail} {self.date} {self.status}"
+
+class Status(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.id} {self.name} {self.description}"
+
+class Log(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="product_log")
+    status_id = models.ForeignKey(Status, on_delete=models.PROTECT, related_name="status")
+    date = models.DateField()
+    def __str__(self):
+        return f"{self.id} {self.product_id} {self.status_id} {self.date}"
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=25)
+    x = models.SmallIntegerField(default=0)
+    y = models.SmallIntegerField(default=0)
+    z = models.SmallIntegerField(default=0)
+    status = models.BooleanField(default=1)
+    def __str__(self):
+        return f"{self.name}"
+
+class Profile(models.Model):
+    name = models.CharField(max_length=35)
+    item_code = models.CharField(max_length=30)
+    item_cod_desc = models.CharField(max_length=64)
+    link = models.CharField(max_length=512)
+    level = models.SmallIntegerField()
+    status = models.BooleanField(default=1)
+    def __str__(self):
+        return f"{self.item_code} | {self.item_cod_desc} | {self.level}"
+
+class Profile_Location(models.Model):
+    profile_id = models.ForeignKey(Profile, on_delete=models.PROTECT, related_name="profile")
+    location_id = models.ForeignKey(Location, on_delete=models.PROTECT, related_name="location_profile")
+    step = models.SmallIntegerField()
+    def __str__(self):
+        return f"{self.profile_id} {self.location_id} {self.step}"
+
+class Location_User(models.Model):
+    location_id = models.ForeignKey(Location, on_delete=models.PROTECT, related_name="location_user")
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT, related_name="user")
+    def __str__(self):
+        return f"{self.location_id} {self.user_id}"
+
